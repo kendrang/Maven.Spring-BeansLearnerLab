d252cb7d4ca3a9d8e2f61720bc50ed749db8d76b
kendrang <kendrang@users.noreply.github.com>
2019-12-12 15:11:16 -0500

    fixed errors

--- demo/src/main/java/com/example/demo/models/Alumni.java
+++ demo/src/main/java/com/example/demo/models/Alumni.java
@@ -1,13 +1,40 @@
 package com.example.demo.models;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
 
 @Component
 public class Alumni {
 
+    @Autowired
+    @Qualifier( "previousStudents")
+    private Students previousStudents;
 
+    @Autowired
+    @Qualifier("instructors")
+    private Instructors instructors;
 
+    @PostConstruct
+    public void executeBootcamp() {
+        //int numberOfInstructors = instructors.size();
+        //    int numberOfStudents = students.size();
+        //    double numberOfHoursToTeachEachStudent = 1200;
+        //    double numberOfHoursToTeach = numberOfHoursToTeachEachStudent * numberOfStudents;
+        //    double numberOfHoursPerInstructor = numberOfHoursToTeach / numberOfInstructors;
+        for(Instructor instructor: instructors){
+            //instructor.lecture(previousStudents, (1200.0 * previousStudents.size()) / instructors.size());
+        }
 
+    }
+    public Students getPreviousStudents() {
+        return previousStudents;
+    }
+
+    public Instructors getInstructors() {
+        return instructors;
+    }
 }
--- demo/src/main/java/com/example/demo/models/People.java
+++ demo/src/main/java/com/example/demo/models/People.java
@@ -1,59 +1,51 @@
 package com.example.demo.models;
 
 import java.util.*;
-import java.util.function.Consumer;
 
-public abstract class People <PersonType extends Person> implements Iterable<PersonType>{
-
+public abstract class People<PersonType extends Person> implements Iterable<PersonType> {
     private List<PersonType> personList;
 
-    public People(List<PersonType> people) { this.personList = people;
+    public People(List<PersonType> personList){
+        this.personList = personList;
     }
 
-    public void add (PersonType person){
+    public void add(PersonType person){
         personList.add(person);
     }
 
-    public void remove (PersonType person){
+    public void remove(PersonType person){
         personList.remove(person);
     }
 
-    public Integer size (List<PersonType> personList){
+    public Integer size(){
         return personList.size();
     }
 
-    public void clear (List<PersonType> personList){
+    public void clear(){
         personList.clear();
     }
 
-    public void addAll(List<PersonType>personList , Iterable<PersonType> toAdd){
-        personList.addAll((Collection<? extends PersonType>) toAdd);
+    public void addAll(Iterable<PersonType> personList){
+        this.personList.addAll((Collection<? extends PersonType>) personList);
     }
 
-    public PersonType findById (Long id){
-       for (PersonType p : personList){
-           if(p.getId().equals(id)){
-               return p;
-           }
-       }return null;
+    public PersonType findById(long id){
+        for (PersonType person : personList) {
+            if (person.getId() == id){
+                return person;
+            }
+        }
+        return null;
     }
 
-    public List<PersonType> findAll (){
-        return this.personList;
+    public void findAll(){
+        personList.stream()
+                .map(personType -> personList)
+                .forEach(System.out::println);
     }
 
     @Override
     public Iterator<PersonType> iterator() {
         return personList.iterator();
-    }
-
-    @Override
-    public void forEach(Consumer<? super PersonType> action) {
-
-    }
-
-    @Override
-    public Spliterator<PersonType> spliterator() {
-        return personList.spliterator();
     }
 }
